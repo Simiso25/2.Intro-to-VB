e2ba14b536f2f4a5b84017821a264ad39c1f3fb6
Lenovo-User <Lenovo-User@DESKTOP-74N9OPR>
2024-01-18 09:21:03 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        ComboBox1 = New ComboBox()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label6 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(176, 40)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(39, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Name"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(176, 74)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(54, 15)
+        Label2.TabIndex = 0
+        Label2.Text = "Surname"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(176, 105)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(28, 15)
+        Label3.TabIndex = 0
+        Label3.Text = "Age"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(176, 138)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(62, 15)
+        Label4.TabIndex = 0
+        Label4.Text = "Profession"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(176, 173)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(45, 15)
+        Label5.TabIndex = 0
+        Label5.Text = "Gender"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(199, 267)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(144, 41)
+        Button1.TabIndex = 1
+        Button1.Text = "Welcome Button"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(350, 267)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(115, 41)
+        Button2.TabIndex = 1
+        Button2.Text = "Close"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Teacher", "Lecturer", "Nurse", "Police"})
+        ComboBox1.Location = New Point(280, 138)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 2
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(280, 173)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(63, 19)
+        RadioButton1.TabIndex = 3
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Female"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(280, 198)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(51, 19)
+        RadioButton2.TabIndex = 3
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Male"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(280, 223)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(55, 19)
+        RadioButton3.TabIndex = 3
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Other"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(280, 37)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(280, 71)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 4
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(280, 105)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 4
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label6.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        Label6.Location = New Point(189, 9)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(369, 25)
+        Label6.TabIndex = 5
+        Label6.Text = "Welcome to the Mindworx SignUp Page"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label6)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(RadioButton3)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(ComboBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        ForeColor = Color.Black
+        Name = "Form1"
+        Text = "SignUp"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label6 As Label
+
+End Class
